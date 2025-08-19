0dda59bfedaaff88f3c04dc9f3a25dfc198bdfcc
liukuang <liukuang.ai@gmail.com>
2025-08-20 01:00:07 +0800

    Add samples

--- diffusion.py
+++ diffusion.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 from itertools import pairwise
 from torch.utils.data import Dataset, DataLoader
@@ -14,7 +15,7 @@ class DatasaurusDozen(Dataset):
         df = pd.read_csv(csv_file, delimiter="\t")
         points = df[df["dataset"] == dataset][["x", "y"]]
         points = (points.to_numpy() - 50.) / 50.
-        self.points = torch.from_numpy(points)
+        self.points = torch.from_numpy(points).float()
 
     def __getitem__(self, i: int) -> torch.Tensor:
         return self.points[i % len(self.points)]
@@ -39,6 +40,11 @@ class NoiseScheduler:
     def sample_batch(self, batch_size: int) -> torch.Tensor:
         return self[torch.randint(len(self), (batch_size,))]
 
+    def sample_sigmas(self, steps: int) -> torch.Tensor:
+        indices = list((len(self) * (1 - np.arange(0, steps)/steps))
+                       .round().astype(np.int64) - 1)
+        return self[indices + [0]]
+
 
 class SimpleMLP(nn.Module):
     def __init__(self):
@@ -50,11 +56,11 @@ class SimpleMLP(nn.Module):
         layers.append(nn.Linear(dims[-1], 2))
         self.layers = nn.Sequential(*layers)
 
-    def forward(self, x, sigma):
+    def forward(self, x: torch.Tensor, sigma: torch.Tensor) -> torch.Tensor:
         """
         Args:
           x: (torch.tensor) input tensor, sized [N,2].
-          sigma: (torch.tensor) input noise, sized [N,].
+          sigma: (torch.tensor) input noise level, sized [N,].
         """
         sigma_embs = get_sigma_embs(sigma)  # [N,2]
         inputs = torch.cat([x, sigma_embs], dim=1)  # [N,4]
@@ -91,9 +97,26 @@ def generate_sample(x0, noise_scheduler):
     return x0, sigma, eps
 
 
+def samples(model: nn.Module,
+            sigmas: torch.Tensor,
+            gam: float = 1.,
+            mu: float = 0.,
+            batch_size: int = 1):
+    model.eval()
+    xt = torch.randn(batch_size, 2) * sigmas[0]
+    eps = None
+    for i, (sig, sig_prev) in enumerate(pairwise(sigmas)):
+        eps_prev, eps = eps, model(xt, sig.repeat(len(xt)))
+        eps_av = eps * gam + eps_prev * (1-gam) if i > 0 else eps
+        sig_p = (sig_prev/sig**mu)**(1/(1-mu))
+        eta = (sig_prev**2 - sig_p**2).sqrt()
+        xt = xt - (sig - sig_p) * eps_av + eta * torch.randn(len(xt), 2)
+        yield xt
+
+
 # Prepare data
 dataset = DatasaurusDozen(csv_file="./data/DatasaurusDozen.tsv", dataset="dino")
-dataloader = DataLoader(dataset, batch_size=143)
+dataloader = DataLoader(dataset, batch_size=2130)
 
 # Noise scheduler
 noise_scheduler = NoiseScheduler(sigma_min=0.02, sigma_max=10.0, T=200)
@@ -102,19 +125,26 @@ noise_scheduler = NoiseScheduler(sigma_min=0.02, sigma_max=10.0, T=200)
 model = SimpleMLP()
 
 # Train
-num_epochs = 100
-criterion = nn.MSELoss()
-optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4)
-scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=num_epochs)
-model.train()
-for epoch in range(num_epochs):
-    print(f"Epoch: {epoch}")
-    for x0 in dataloader:
-        optimizer.zero_grad()
-        x0, sigma, eps = generate_sample(x0.float(), noise_scheduler)
-        y = model(x0 + sigma[:, None] * eps, sigma)
-        loss = criterion(y, eps)
-        loss.backward()
-        optimizer.step()
-        print(f"Loss: {loss.item():.2f}")
-    scheduler.step()
+# num_epochs = 1000
+# criterion = nn.MSELoss()
+# optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
+# scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=num_epochs)
+# model.train()
+# for epoch in range(num_epochs):
+#     print(f"Epoch: {epoch}")
+#     for x0 in dataloader:
+#         optimizer.zero_grad()
+#         x0, sigma, eps = generate_sample(x0, noise_scheduler)
+#         y = model(x0 + sigma[:, None] * eps, sigma)
+#         loss = criterion(y, eps)
+#         loss.backward()
+#         optimizer.step()
+#         print(f"Loss: {loss.item():.2f}")
+#     scheduler.step()
+# torch.save(model.state_dict(), "./checkpoint/model.pth")
+model.load_state_dict(torch.load("./checkpoint/model.pth"))
+*xts, x0 = samples(model, noise_scheduler.sample_sigmas(20), batch_size=1500, gam=2, mu=0)
+print(x0.shape)
+x0 = x0.detach().numpy()
+plt.scatter(x0[:, 0], x0[:, 1], marker='.')
+plt.show()
